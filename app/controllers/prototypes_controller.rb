cfeafe3db21e56ceb095327e4d4c49bb5e07fa9a
kawayuyu <96399944+kawayuyu@users.noreply.github.com>
2022-01-14 10:42:00 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :index, :new]
-  before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new,:edit,:destroy]
+  before_action :move_to_index , only: [:edit]
   
   def index
     @prototypes = Prototype.all
@@ -50,9 +50,9 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
   
-  def move_to_index
-    prototype = Prototype.find(params[:id])
-    if prototype.user_id != current_user.id
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
       redirect_to action: :index
     end
   end
